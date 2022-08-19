1e5401dfb6cdb5dd86bf0bd73219fe2f043ef012
Shivam Raj <butwhycode@gmail.com>
2022-08-19 11:49:14 +0530

    setup all jobs model

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+require("dotenv").config();
+const connectDB = require("./db/connect");
+const AllJob = require("./models/allJob");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -9,6 +12,26 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(3000, () => {
-  console.log("Server is starting at port 3000");
+app.get("/newJob", async (req, res) => {
+  const newJob = new AllJob({
+    title: "backend",
+    description: "backend developer in xyz",
+    location: "bangalore",
+    eligible: "experience of  5 years required",
+  });
+  await newJob.save();
+  res.send(newJob);
 });
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is starting at port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
